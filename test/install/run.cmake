ff1a3e5e896bd0375adda8852255ea086784b0a4
changyun <chang_yun@outlook.com>
2026-09-10 05:07:21 +0000

    fix: preserve fmt fallback in installed packages

--- test/install/run.cmake
+++ test/install/run.cmake
@@ -11,9 +11,13 @@ file(MAKE_DIRECTORY "${WORK_DIR}")
 run("${CMAKE_COMMAND}" --install "${CHRONON_BUILD_DIR}"
     --prefix "${WORK_DIR}/staging" --config "${CONFIG}")
 file(RENAME "${WORK_DIR}/staging" "${WORK_DIR}/relocated")
-run("${CMAKE_COMMAND}" -S "${CONSUMER_SOURCE}" -B "${WORK_DIR}/consumer"
-    "-DCMAKE_PREFIX_PATH=${WORK_DIR}/relocated" "-DCMAKE_CXX_COMPILER=${CXX}"
-    -DCMAKE_FIND_USE_PACKAGE_REGISTRY=OFF -DCMAKE_FIND_USE_SYSTEM_PACKAGE_REGISTRY=OFF)
-run("${CMAKE_COMMAND}" --build "${WORK_DIR}/consumer" --config "${CONFIG}" -j2)
-run("${CMAKE_CTEST_COMMAND}" --test-dir "${WORK_DIR}/consumer" -C "${CONFIG}"
-    --output-on-failure)
+foreach(fmt_fallback OFF ON)
+    set(consumer_build "${WORK_DIR}/consumer-${fmt_fallback}")
+    run("${CMAKE_COMMAND}" -S "${CONSUMER_SOURCE}" -B "${consumer_build}"
+        "-DCMAKE_PREFIX_PATH=${WORK_DIR}/relocated" "-DCMAKE_CXX_COMPILER=${CXX}"
+        "-DCHRONON_TEST_FMT_FALLBACK=${fmt_fallback}"
+        -DCMAKE_FIND_USE_PACKAGE_REGISTRY=OFF -DCMAKE_FIND_USE_SYSTEM_PACKAGE_REGISTRY=OFF)
+    run("${CMAKE_COMMAND}" --build "${consumer_build}" --config "${CONFIG}" -j2)
+    run("${CMAKE_CTEST_COMMAND}" --test-dir "${consumer_build}" -C "${CONFIG}"
+        --output-on-failure)
+endforeach()
